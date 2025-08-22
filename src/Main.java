bdf77b959ee3bfada5985a59f45867ad24eb6053
Nurmuhammad Abdirashidov <nurmuhammadadeveloper@gmail.com>
2025-08-22 20:36:01 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+       var main = new Main();
+        main.workCounter("hello world qwerty world", 2);
+    }
+
+    public void workCounter(String str, int n) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String st: str.split("\\s+")) {
+            var count = map.getOrDefault(st,0);
+
+            if(n == ++ count)  {
+                System.out.println(st);
+                return;
+            }
+
+            map.put(st, count);
+        }
+        System.out.println("The text didn't have this count words");
+    }
+}
